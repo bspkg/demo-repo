41bcce4541972e6313466ccc342c99341fa0f4f1
bspkg <134151959+bspkg@users.noreply.github.com>
2024-02-04 00:57:25 +0000

    Added error condition

--- summon-robot.py
+++ summon-robot.py
@@ -6,7 +6,10 @@ def summon_bot(button_colour):
         return 'C3PO'  # If the button colour is Gold, return 'C3PO'
     elif button_colour == 'Blue':
         return 'R2D2'  # If the button colour is Blue, return 'R2D2'
+    else:
+        return 'Unrecognised button colour' # If the button colour is neither Gold nor Blue, return 'Unrecognised button colour'
 
 # Example usage
 print(summon_bot('Gold'))  # Output: C3PO
 print(summon_bot('Blue'))  # Output: R2D2
+print(summon_bot('Red'))  # Output: Unrecognised button colour
